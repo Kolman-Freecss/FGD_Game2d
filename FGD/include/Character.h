9055ceb03e42988f28f4c82d1ae5fbbcb734891c
Sergio225 <s3rg10.martinez.roman@gmail.com>
2018-04-17 16:57:47 +0200

    Commit Without Attrib

---
+++ FGD/include/Character.h
@@ -0,0 +1,16 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+
+class Character
+{
+    public:
+        Character();
+        virtual ~Character();
+
+    protected:
+
+    private:
+};
+
+#endif // CHARACTER_H
