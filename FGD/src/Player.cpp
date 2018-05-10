323eaa1516e15e41b0ec9efdbaa45ad5dd042e4e
Sergio225 <s3rg10.martinez.roman@gmail.com>
2018-05-10 20:25:57 +0200

    Tabs

--- FGD/src/Player.cpp
+++ FGD/src/Player.cpp
@@ -17,25 +17,25 @@ Player::Player(BITMAP ***animations, int health, int damage, double speed, int s
 void Player::keyboard()
 {
     if ( key[KEY_UP] )
-          {
-               this->y-= this->speed;
-          }
-          if ( key[KEY_DOWN] )
-          {
-               this->y+= this->speed;
-          }
-          if ( key[KEY_LEFT] )
-          {
-               this->x-= this->speed;
-          }
-          if ( key[KEY_RIGHT] )
-          {
-               this->x+= this->speed;
-          }
+    {
+        this->y-= this->speed;
+    }
+    if ( key[KEY_DOWN] )
+    {
+        this->y+= this->speed;
+    }
+    if ( key[KEY_LEFT] )
+    {
+        this->x-= this->speed;
+    }
+    if ( key[KEY_RIGHT] )
+    {
+        this->x+= this->speed;
+    }
 
-          // limites
-          if ( this->x < 0 ) this->x = 0;
-          if ( this->x+this->width > 800 ) this->x = 800-this->width;
-          if ( this->y < 0 ) this->y = 0;
-          if ( this->y+this->height > 600 ) this->y = 600-this->height;
+    // limites
+    if ( this->x < 0 ) this->x = 0;
+    if ( this->x+this->width > 800 ) this->x = 800-this->width;
+    if ( this->y < 0 ) this->y = 0;
+    if ( this->y+this->height > 600 ) this->y = 600-this->height;
 }
