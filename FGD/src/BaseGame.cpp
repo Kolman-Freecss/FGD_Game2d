e123f23cbc88e4d81679e9302692d4d4d31fe63a
Unknown <teammoya@hotmail.com>
2018-05-24 15:50:04 +0200

    Auto stash before merge of "Develop" and "origin/Develop"

--- FGD/src/BaseGame.cpp
+++ FGD/src/BaseGame.cpp
@@ -472,19 +472,19 @@ void BaseGame::drawEnemyHUD(Enemy *enemy) {
     int maxSizeBar = enemy->getWidth();
     //barras de vida enemigo
     int posXbars = 0;
-    int posYhp = -5;
-    int posYsh = -15;
+    int posYhp = -15;
+    int posYsh = -s5;
 
     BITMAP *BlackBar = load_bitmap("src\\Resources\\hpbarblack.bmp",NULL);
-    masked_blit(BlackBar, this->game->getBuffer(), 0, 0,  enemy->getX()+posXbars-1, enemy->getY()-posYhp-1, maxSizeBar +2, 7);
-    masked_blit(BlackBar, this->game->getBuffer(), 0, 0,  enemy->getX()+posXbars-1, enemy->getY()-posYsh-1, maxSizeBar +2, 7);
+    masked_blit(BlackBar, this->game->getBuffer(), 0, 0,  enemy->getX()+posXbars-1, enemy->getY()+posYhp-1, maxSizeBar +2, 7);
+    masked_blit(BlackBar, this->game->getBuffer(), 0, 0,  enemy->getX()+posXbars-1, enemy->getY()+posYsh-1, maxSizeBar +2, 7);
 
 
     BITMAP *bitmapHealth = load_bitmap("src\\Resources\\health.bmp",NULL);
-    masked_blit(bitmapHealth, this->game->getBuffer(), 0, 0, enemy->getX()+posXbars, enemy->getY()-posYhp, (enemy->getHealth()*100)/maxSizeBar, 5);
+    masked_blit(bitmapHealth, this->game->getBuffer(), 0, 0, enemy->getX()+posXbars, enmy->getY()+posYhp, (enemy->getHealth()*100)/maxSizeBar, 5);
 
     BITMAP *bitmapShield = load_bitmap("src\\Resources\\shield.bmp",NULL);
-    masked_blit(bitmapShield, this->game->getBuffer(), 0, 0, enemy->getX()+posXbars, enemy->getY()-posYsh, (enemy->getShield()*100)/maxSizeBar, 5);
+    masked_blit(bitmapShield, this->game->getBuffer(), 0, 0, enemy->getX()+posXbars, enemy->getY()+posYsh, (enemy->getShield()*100)/maxSizeBar, 5);
 
 }
 
